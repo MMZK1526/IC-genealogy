e92ccae756539b2f549a99f9d62f21ded9da2173
mmzk1526 <yc4120@ic.ac.uk>
2022-10-10 11:58:08 +0100

    [mmzk] (init) Set up repo and consistent linefeed

---
+++ genealogy-b/src/main/kotlin/mmzk/genealogy/Application.kt
@@ -0,0 +1,14 @@
+package mmzk.genealogy
+
+import mmzk.genealogy.plugins.configureRouting
+import mmzk.genealogy.plugins.configureSerialization
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
