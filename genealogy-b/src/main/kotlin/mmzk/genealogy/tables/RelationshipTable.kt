83c8a4aa9dd7a940eacc71e13c2143beb76fe66c
mmzk1526 <yc4120@ic.ac.uk>
2022-10-13 23:56:43 +0100

    [mmzk] (feat) Change the primary key type for "relationship_type" to String for better extensibility

--- genealogy-b/src/main/kotlin/mmzk/genealogy/tables/RelationshipTable.kt
+++ genealogy-b/src/main/kotlin/mmzk/genealogy/tables/RelationshipTable.kt
@@ -1,14 +1,12 @@
 package mmzk.genealogy.tables
 
 import org.jetbrains.exposed.dao.IntIdTable
-import org.jetbrains.exposed.sql.Table
-
-object RelationshipTable: IntIdTable() {
+object RelationshipTable : IntIdTable() {
     val person1 = reference("person1", IndividualTable.id)
     val person2 = reference("person2", IndividualTable.id)
     val type = reference("type", RelationshipTypeTable.id)
 }
 
-object RelationshipTypeTable: IntIdTable("relationship_type") {
+object RelationshipTypeTable : StringIdTable("relationship_type") {
     val name = varchar("relation_name", 255)
 }
