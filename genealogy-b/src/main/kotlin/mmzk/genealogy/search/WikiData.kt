cc13feedee8dbca3ba434cec837f2f9f2dca0ebf
mmzk1526 <yc4120@ic.ac.uk>
2022-10-17 18:04:23 +0100

    [mmzk] (refactor) More optimisation on quest efficiency by increading parallelism

--- genealogy-b/src/main/kotlin/mmzk/genealogy/search/WikiData.kt
+++ genealogy-b/src/main/kotlin/mmzk/genealogy/search/WikiData.kt
@@ -11,6 +11,7 @@ import mmzk.genealogy.dto.RelationshipDTO
 import com.google.gson.JsonElement
 import com.google.gson.JsonParser
 import io.ktor.client.statement.*
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import mmzk.genealogy.tables.Fields
@@ -24,56 +25,88 @@ object WikiData {
         this.engine { this.threadsCount = 8 }
     }
 
+    // Translate a WikiData ID into a Database ID
     fun makeID(id: String): String = "WD-$id"
 
     suspend fun query(id: String): IndividualDTO? {
-        suspend fun getValueAndClaims(id: String) = coroutineScope {
-            val url =
-                "https://www.wikidata.org/w/api.php?action=wbgetentities&ids=$id&props=labels%7Cclaims&languages=en&formatversion=2&format=json"
-            val response = client.get(url)
-            val entity = JsonParser.parseString(response.bodyAsText()).asObjectOrNull?.get("entities")
-                ?.asObjectOrNull?.get(id)
-            val value = entity
-                ?.asObjectOrNull?.get("labels")
-                ?.asObjectOrNull?.get("en")
-                ?.asObjectOrNull?.get("value")
-                ?.asStringOrNull
-            val claims = entity?.asObjectOrNull?.get("claims")
-            value to claims
+        // Get the values and claims corresponding to the list of WikiData IDs
+        suspend fun getValuesAndClaims(ids: List<String>) = coroutineScope {
+            if (ids.isEmpty()) {
+                mapOf()
+            } else {
+                val idsStr = ids.reduce { id1, id2 -> "$id1|$id2" }
+                val url =
+                    "https://www.wikidata.org/w/api.php?action=wbgetentities&ids=$idsStr&props=labels%7Cclaims&languages=en&formatversion=2&format=json"
+                val response = client.get(url)
+
+                ids.map {
+                    it to run {
+                        val entity = JsonParser.parseString(response.bodyAsText()).asObjectOrNull?.get("entities")
+                            ?.asObjectOrNull?.get(it)?.asObjectOrNull
+                        entity
+                            ?.get("labels")
+                            ?.asObjectOrNull?.get("en")
+                            ?.asObjectOrNull?.get("value")
+                            ?.asStringOrNull to entity?.get("claims")
+                    }
+                }.associate { it }
+            }
         }
 
-        suspend fun JsonElement.readPropertyReference(callback: suspend (String?, JsonElement?) -> String? = { str, _ -> str }): Pair<String?, JsonElement?> =
+        suspend fun JsonElement.readPropertiesReferences(queries: Map<String, (suspend (String?, JsonElement?) -> String?)?>) =
             coroutineScope {
-                val dataValue =
-                    this@readPropertyReference.asObjectOrNull?.get("mainsnak")?.asObjectOrNull?.get("datavalue")
-                val dataValueMap = dataValue?.asObjectOrNull
-                val references = this@readPropertyReference.asObjectOrNull?.get("references")
-                when (dataValueMap?.get("type")?.asStringOrNull) {
-                    Fields.string -> dataValueMap["value"].asStringOrNull to references
-                    Fields.time -> dataValue.asObjectOrNull?.get("value")?.asObjectOrNull?.get("time")?.asStringOrNull to references
-                    Fields.wikiBaseEntityId -> dataValue.asObjectOrNull?.get("value")?.asObjectOrNull?.get("id")?.asStringOrNull?.let {
-                        val (value, claims) = getValueAndClaims(it)
-                        callback(value, claims)
-                    } to references
-                    else -> null to references
+                val knownResults = mutableMapOf<String, Pair<String?, JsonElement?>>();
+                val deferrals = mutableMapOf<String, Deferred<Pair<String?, JsonElement?>>>();
+                val indirections = mutableMapOf<String, Pair<String?, JsonElement?>>();
+                for (query in queries) {
+                    val data =
+                        this@readPropertiesReferences.asObjectOrNull?.get(query.key)?.asArrayOrNull?.get(0)?.asObjectOrNull
+                    val references = data?.get("references")
+                    val dataValue = data?.get("mainsnak")?.asObjectOrNull?.get("datavalue")?.asObjectOrNull
+                    when (dataValue?.get("type")?.asStringOrNull) {
+                        Fields.string -> deferrals[query.key] = async {
+                            val propertyValue = dataValue["value"].asStringOrNull
+                            (query.value?.invoke(propertyValue, null) ?: propertyValue) to references
+                        }
+                        Fields.time -> deferrals[query.key] = async {
+                            val timestamp =
+                                dataValue.asObjectOrNull?.get("value")?.asObjectOrNull?.get("time")?.asStringOrNull
+                            (query.value?.invoke(timestamp, null)
+                                ?: timestamp) to references
+                        }
+                        Fields.wikiBaseEntityId -> indirections[query.key] =
+                            dataValue.asObjectOrNull?.get("value")?.asObjectOrNull?.get("id")?.asStringOrNull to references
+                    }
                 }
-            }
 
-        suspend fun JsonElement.readProperty(callback: suspend (String?, JsonElement?) -> String? = { str, _ -> str }): String? =
-            coroutineScope {
-                val (str, _) = readPropertyReference(callback)
-                str
+                val valueClaims = getValuesAndClaims(indirections.values.mapNotNull { it.first }.toList())
+                for (i in indirections) {
+                    deferrals[i.key] = async {
+                        i.value.first?.let {
+                            queries[i.key]?.invoke(
+                                valueClaims[it]?.first,
+                                valueClaims[it]?.second
+                            ) ?: valueClaims[it]?.first
+                        } to indirections[i.key]?.second
+                    }
+                }
+
+                for (d in deferrals) {
+                    knownResults[d.key] = d.value.await()
+                }
+
+                knownResults
             }
 
         suspend fun countryOfPlace(place: String?, claims: JsonElement?): String? = coroutineScope {
             val country =
-                claims?.asObjectOrNull?.get(Fields.country)?.asArrayOrNull?.get(0)?.readProperty()
+                claims?.asObjectOrNull?.readPropertiesReferences(mapOf(Fields.country to null))?.values?.first()?.first
             place?.let { p -> country?.let { "$p, $it" } ?: p }
         }
 
         return coroutineScope {
             try {
-                val (name, claims) = getValueAndClaims(id)
+                val (name, claims) = getValuesAndClaims(listOf(id)).values.first()
                 val gender =
                     when (claims?.asObjectOrNull?.get(Fields.gender)?.asArrayOrNull?.get(0)?.asObjectOrNull?.get("mainsnak")?.asObjectOrNull?.get(
                         "datavalue"
@@ -84,29 +117,37 @@ object WikiData {
                         Fields.male -> 'M'
                         else -> 'U'
                     }
-                val placeOfBirthAsync = async {
-                    claims?.asObjectOrNull?.get(Fields.placeOfBirth)?.asArrayOrNull?.get(0)
-                        ?.readProperty(::countryOfPlace)
-                }
-                val placeOfDeathAsync =
-                    async {
-                        claims?.asObjectOrNull?.get(Fields.placeOfDeath)?.asArrayOrNull?.get(0)
-                            ?.readProperty(::countryOfPlace)
-                    }
-                val dateOfBirthAsync =
-                    async { claims?.asObjectOrNull?.get(Fields.dateOfBirth)?.asArrayOrNull?.get(0)?.readProperty() }
-                val dateOfDeathAsync =
-                    async { claims?.asObjectOrNull?.get(Fields.dateOfDeath)?.asArrayOrNull?.get(0)?.readProperty() }
+                val queries =
+                    mapOf(
+                        Fields.dateOfBirth to null,
+                        Fields.dateOfDeath to null,
+                        Fields.placeOfBirth to ::countryOfPlace,
+                        Fields.placeOfDeath to ::countryOfPlace
+                    )
+                val queryResults = claims?.readPropertiesReferences(queries)
+//                val placeOfBirthAsync = async {
+//                    claims?.asObjectOrNull?.get(Fields.placeOfBirth)?.asArrayOrNull?.get(0)
+//                        ?.readProperty(::countryOfPlace)
+//                }
+//                val placeOfDeathAsync =
+//                    async {
+//                        claims?.asObjectOrNull?.get(Fields.placeOfDeath)?.asArrayOrNull?.get(0)
+//                            ?.readProperty(::countryOfPlace)
+//                    }
+//                val dateOfBirthAsync =
+//                    async { claims?.asObjectOrNull?.get(Fields.dateOfBirth)?.asArrayOrNull?.get(0)?.readProperty() }
+//                val dateOfDeathAsync =
+//                    async { claims?.asObjectOrNull?.get(Fields.dateOfDeath)?.asArrayOrNull?.get(0)?.readProperty() }
                 val personalName = "TODO" // TODO
                 name?.let {
                     IndividualDTO(
                         id,
                         it,
                         personalName,
-                        dateOfBirthAsync.await(),
-                        dateOfDeathAsync.await(),
-                        placeOfBirthAsync.await(),
-                        placeOfDeathAsync.await(),
+                        queryResults?.get(Fields.dateOfBirth)?.first,
+                        queryResults?.get(Fields.dateOfDeath)?.first,
+                        queryResults?.get(Fields.placeOfBirth)?.first,
+                        queryResults?.get(Fields.placeOfDeath)?.first,
                         gender,
                         false
                     )
