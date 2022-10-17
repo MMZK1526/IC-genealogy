3bdf904892dc779c348e9b766c6e111932333471
Piotr Błaszyk <piopio555888@gmail.com>
2022-10-17 10:53:36 +0100

    [psb120] (feat) Added dropdown bar for disambiguation to let the user choose the person to analyze

--- genealogy-f/src/App.js
+++ genealogy-f/src/App.js
@@ -13,16 +13,28 @@ class App extends React.Component {
 class NameForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: '', disambiguationJson: '', relationsJson: ''};
+        this.state = {
+            initialName: '',
+            disambiguationJson: [],
+            chosenId: '',
+            relationsJson: [],
+        };
         this.requests = new Requests();
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleSubmit2 = this.handleSubmit2.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({initialName: event.target.value});
     }
+
+    handleChange2(event) {
+        this.setState({chosenId: event.target.value});
+    }
+
 
     render() {
         return (
@@ -30,19 +42,35 @@ class NameForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         Name:
-                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                        <input type="text" value={this.state.initialName} onChange={this.handleChange}/>
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
                 <div>
                     {this.state.disambiguationJson
-                        ? this.state.disambiguationJson
+                        ? JSON.stringify(this.state.disambiguationJson)
                         : 'No data fetched'
                     }
                 </div>
+                {this.state.disambiguationJson
+                    ? <form onSubmit={this.handleSubmit2}>
+                        <label>
+                            Disambiguation:
+                            <select value={this.state.chosenId} onChange={this.handleChange2}>
+                                {
+                                    this.state.disambiguationJson.map((x) =>
+                                        <option value={x.id} key={x.id}>{x.name}</option>
+                                    )
+                                }
+                            </select>
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                    : 'No data fetched'
+                }
                 <div>
                     {this.state.relationsJson
-                        ? this.state.relationsJson
+                        ? JSON.stringify(this.state.relationsJson)
                         : 'No data fetched'
                     }
                 </div>
@@ -52,18 +80,26 @@ class NameForm extends React.Component {
 
     async handleSubmit(event) {
         event.preventDefault();
-        await this.requests.search(this.state.value).then(r => {
+        await this.requests.search(this.state.initialName).then(r => {
             this.setState({
-                disambiguationJson: JSON.stringify(r)
+                disambiguationJson: r,
+                chosenId: r[0].id
             });
         });
-        await this.requests.relations().then(r => {
+
+    }
+
+    async handleSubmit2(event) {
+        event.preventDefault();
+        await this.requests.relations({id: this.state.chosenId}).then(r => {
             this.setState({
-                relationsJson: JSON.stringify(r)
+                relationsJson: r
             });
         });
     }
 
 }
+
+
 
 export default App;
