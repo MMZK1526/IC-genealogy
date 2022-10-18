7fed40f2070d9ead5a386d61a860773cc5c22bb7
Mulang Su <sumulang.uk@gmail.com>
2022-10-18 15:29:12 +0100

    [ms4620] (refactor) moved /search endpoint logic to Database.kt, also renamed searchId

--- genealogy-b/src/main/kotlin/mmzk/genealogy/search/Database.kt
+++ genealogy-b/src/main/kotlin/mmzk/genealogy/search/Database.kt
@@ -4,15 +4,17 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import mmzk.genealogy.dao.Individual
 import mmzk.genealogy.dao.Relationship
+import mmzk.genealogy.dto.AdditionalProperty
 import mmzk.genealogy.dto.IndividualDTO
 import mmzk.genealogy.dto.RelationsResponse
 import mmzk.genealogy.dto.RelationshipDTO
+import mmzk.genealogy.tables.AdditionalPropertiesTable
+import mmzk.genealogy.tables.IndividualTable
 import mmzk.genealogy.tables.RelationshipTable
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.VarCharColumnType
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.castTo
-import org.jetbrains.exposed.sql.or
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.regexp
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.net.URI
 
@@ -38,7 +40,18 @@ object Database {
         return HikariDataSource(config)
     }
 
-    fun searchId(id: String, typeFilter: List<String>?): RelationsResponse? = transaction TRANS@{
+    fun findPersonByName(name: String): List<IndividualDTO> = transaction {
+        val daos = Individual.find { IndividualTable.name.regexp("(?i).*$name.*") }
+            .limit(20)
+        val additionalPropertiesByIndividual = AdditionalPropertiesTable.select {
+            AdditionalPropertiesTable.individualId.inList(daos.map { it.id.value })
+        }.groupBy { it[AdditionalPropertiesTable.individualId] }
+        daos.map { dao ->
+            IndividualDTO(dao, additionalPropertiesByIndividual[dao.id.value]?.map(::AdditionalProperty) ?: listOf())
+        }
+    }
+
+    fun findRelatedPeople(id: String, typeFilter: List<String>?): RelationsResponse? = transaction TRANS@{
         val target = Individual.findById(id) ?: return@TRANS null
         val relationships = if (typeFilter != null && typeFilter != listOf("all")) {
             Relationship.find {
