9dcb2165117f1bb57649f21711be0841ac89de6a
mm2320 <mm2320@ic.ac.uk>
2022-10-18 23:17:47 +0100

    [mm2320, yj1920] (refactor) extract sidebar

--- genealogy-f/src/App.js
+++ genealogy-f/src/App.js
@@ -6,11 +6,6 @@ import {Sidebar} from './components/sidebar/Sidebar.js';
 import {Requests} from './requests';
 import React from "react";
 
-import {FilterForm} from './components/filter/Filter.js'
-import Button from 'react-bootstrap/Button';
-import "./components/sidebar/Sidebar.css"
-
-
 class App extends React.Component {
     render() {
         return (
@@ -63,42 +58,12 @@ class NameForm extends React.Component {
     render() {
         return (
             <div className='App'>
-                {/* <form onSubmit={this.handleSearchSubmit}> */}
-                    <div className='sidebar'>
-                        <div className='name-field'>
-                            <FilterForm title="Name :" placeholder="Name" type="text" onChange={this.handleChangeInitialName}/>
-                        </div>
-                        <div className='date-from-field'>
-                            <FilterForm title="From :" placeholder="Year" type="text" onChange={this.handleChangeFrom}/>
-                        </div>
-                        <div className='date-to-field'>
-                            <FilterForm title="To :" placeholder="Year" type="text" onChange={this.handleChangeTo}/>
-                        </div>
-                        <Button className='apply-button' size='lg' type='primary' onClick={this.handleSearchSubmit}>
-                            Apply filters
-                        </Button>
-                    </div>
-                    {/* <label>
-                        Name:
-                        <input type="text" value={this.state.initialName} onChange={this.handleChangeInitialName}/>
-                    </label>
-
-                    <label> From:
-                        <input type="text" value={this.state.fromYear} onChange={this.handleChangeFrom}/>
-                    </label>
-
-                    <label> To:                      
-                        <input type="text" value={this.state.toYear} onChange={this.handleChangeTo}/>
-                    </label>  
-
-                    <input type="submit" value="Submit"/> */}
-                {/* </form> */}
-                {/*<div>*/}
-                {/*    {this.state.searchJsons*/}
-                {/*        ? this.tableFromArray('disambiguation', this.state.searchJsons)*/}
-                {/*        : 'No data fetched'*/}
-                {/*    }*/}
-                {/*</div>*/}
+                <Sidebar
+                    nameChange={this.handleChangeInitialName}
+                    yearFromChange={this.handleChangeFrom}
+                    yearToChange={this.handleChangeTo}
+                    onClick={this.handleSearchSubmit}
+                />
                 {this.state.searchJsons
                     ? <form onSubmit={this.handleRelationsSubmit}>
                         <label>
@@ -125,14 +90,14 @@ class NameForm extends React.Component {
                             console.assert(Array.isArray(y));
                             return this.tableFromArray(kv[0], y);
                         })
-                        : 'No data fetched'
+                        : ''
                     }
                 </div>
                 <div>
                     {
                         !_.isEmpty(this.state.relationsJson)
                         ? <FamilyTree data={this.state.relationsJson} showChildren={false} />
-                            : 'No data fetched'
+                            : ''
                     }
                 </div>
             </div>
--- genealogy-f/src/components/sidebar/sidebar.js
+++ genealogy-f/src/components/sidebar/sidebar.js
@@ -3,19 +3,19 @@ import {FilterForm} from '../filter/Filter.js'
 import React, { useState } from "react";
 import "./Sidebar.css"
 
-export function Sidebar() {
+export function Sidebar(props) {
   return (
     <div className='sidebar'>
       <div className='name-field'>
-        <FilterForm title="Name :" placeholder="Name" type="text"/>
+        <FilterForm title="Name :" placeholder="Name" type="text" onChange={props.nameChange}/>
       </div>
       <div className='date-from-field'>
-        <FilterForm title="From :" placeholder="Year" type="number"/>
+        <FilterForm title="From :" placeholder="Year" type="number" onChange={props.yearFromChange}/>
       </div>
       <div className='date-to-field'>
-        <FilterForm title="To :" placeholder="Year" type="number"/>
+        <FilterForm title="To :" placeholder="Year" type="number" onChange={props.handleChangeTo}/>
       </div>
-      <Button className='apply-button' size='lg' type='primary'>
+      <Button className='apply-button' size='lg' type='primary' onClick={props.onClick}>
         Apply filters
       </Button>{' '}
     </div>
