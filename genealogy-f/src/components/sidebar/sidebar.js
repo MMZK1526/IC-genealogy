e964618adb9d4e7e3db2ac187c489f8de3b07198
mm2320 <mm2320@ic.ac.uk>
2022-10-18 23:47:05 +0100

    [mm2320, yj1920] (UI) UI for disambiguation

--- genealogy-f/src/components/sidebar/sidebar.js
+++ genealogy-f/src/components/sidebar/sidebar.js
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import {FilterForm} from '../filter/Filter.js'
-import React, { useState } from "react";
+import React from "react";
 import "./Sidebar.css"
 
 export function Sidebar(props) {
@@ -10,15 +10,14 @@ export function Sidebar(props) {
         <FilterForm title="Name :" placeholder="Name" type="text" onChange={props.nameChange}/>
       </div>
       <div className='date-from-field'>
-        <FilterForm title="From :" placeholder="Year" type="number" onChange={props.yearFromChange}/>
+        <FilterForm title="From :" placeholder="Year" type="text" onChange={props.yearFromChange}/>
       </div>
       <div className='date-to-field'>
-        <FilterForm title="To :" placeholder="Year" type="number" onChange={props.handleChangeTo}/>
+        <FilterForm title="To :" placeholder="Year" type="text" onChange={props.yearToChange}/>
       </div>
       <Button className='apply-button' size='lg' type='primary' onClick={props.onClick}>
         Apply filters
-      </Button>{' '}
+      </Button>
     </div>
   );
-  
 }
