079381bd6bcc40638ec4b2921bc4d444ddd3ffe6
Mulang Su <sumulang.uk@gmail.com>
2022-10-31 14:22:31 +0000

    [ms4620] (feature) added qualifiers table

--- genealogy-b/src/main/kotlin/mmzk/genealogy/common/tables/AdditionalPropertiesTable.kt
+++ genealogy-b/src/main/kotlin/mmzk/genealogy/common/tables/AdditionalPropertiesTable.kt
@@ -2,8 +2,17 @@ package mmzk.genealogy.common.tables
 
 import org.jetbrains.exposed.sql.Table
 
-object AdditionalPropertiesTable: Table("additional_properties") {
+object AdditionalPropertiesTable: Table("additional_property") {
     val itemId = varchar("item_id", 32).primaryKey().references(ItemTable.id)
-    val propertyId = varchar("property_id", 255).primaryKey().references(PropertyTypeTable.id)
+    val propertyId = varchar("property_id", 32).primaryKey().references(PropertyTypeTable.id)
     val value = text("value")
+    val valueHash = text("value_hash").primaryKey()
+}
+
+object QualifierTable: Table("qualifier") {
+    val itemId = varchar("item_id", 32).primaryKey().references(AdditionalPropertiesTable.itemId)
+    val qualifierType = varchar("qualifier_type", 32).primaryKey().references(PropertyTypeTable.id)
+    val value = text("value").nullable()
+    val valueHash = text("value_hash").primaryKey().references(AdditionalPropertiesTable.valueHash)
+    val propertyId = varchar("property_id", 32).references(AdditionalPropertiesTable.propertyId)
 }
