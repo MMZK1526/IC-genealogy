b8f23e22360e766d16d57d1cf7030fd865fbb45f
yutongJiang <jiangyt020525@gmail.com>
2022-11-02 20:05:33 +0000

    [yj1920, mm2320] separate columns for attributes

--- genealogy-f/src/components/popup-info/PopupInfo.js
+++ genealogy-f/src/components/popup-info/PopupInfo.js
@@ -8,21 +8,46 @@ function PopupInfo(props) {
         <div className='popup-inner'>
             <button className='close-btn' onClick={props.closePopUp}
             >close</button>
-            <div>
-                {getAdditionalProperties(props.info)}
-            </div>
+            {/* <div class="row"> */}
+            {getAdditionalProperties(props.info)}
+            {/* </div> */}
              
         </div>
     )
 }
 
 function getAdditionalProperties(data) {
-    // Object.keys(Object.fromEntries(data)).map((k) => {
-    //     console.log(k);
-    // })
+    return (
+        <div className="row">
+            <div className="col_key" >
+                {/* <h2>Attributes</h2> */}
+                {getAttrName(data)}
+            </div>
+            <div className="col_val" >
+                {/* <h2>Values</h2> */}
+                {getAttrVal(data)}
+            </div>
+        </div>
+    )
+    // return Object.keys(Object.fromEntries(data)).map((k) => (
+    //     <div key={k}>
+    //         <div key='k'><h4>{k}:  {data.get(k)}</h4></div>
+    //     </div>
+    // ))
+}
+
+function getAttrName(data) {
     return Object.keys(Object.fromEntries(data)).map((k) => (
-        <div key={k}>
-            <div key='k'><h4>{k}:  {data.get(k)}</h4></div>
+        <div id='key' key={k}>
+            <h4>{k}</h4>
+        </div>
+    ))
+}
+
+function getAttrVal(data) {
+    return Object.keys(Object.fromEntries(data)).map((k) => (
+        <div id='val' key={data.get(k)}>
+            <h4>{data.get(k)}</h4>
         </div>
     ))
 }
