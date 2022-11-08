1de7d3e951aa1d7b44da33e28b8bf91523b344cb
Mulang Su <sumulang.uk@gmail.com>
2022-11-08 18:51:40 +0000

    [ms4620] (fix) parent links are not bezier paths anymore!
    Everything is now an arc, approximated by 10 straight lines

--- genealogy-f/src/GenogramTree.js
+++ genealogy-f/src/GenogramTree.js
@@ -495,7 +495,7 @@ export class DiagramWrapper extends React.Component {
         );
 
       this.diagram.linkTemplateMap.add('Marriage',  // for marriage relationships
-        $(SemicircleLink,
+        $(TriangularLink,
           {
             routing: go.Link.Normal,
             fromSpot: go.Spot.Top,
@@ -513,9 +513,7 @@ export class DiagramWrapper extends React.Component {
           fromSpot: go.Spot.Bottom,
           toSpot: go.Spot.Bottom,
           selectable: false,
-          layerName: 'Background',
-          curve: go.Link.Bezier,
-          curviness: 5
+          layerName: 'Background'
         },
         $(go.Shape, { strokeWidth: 1, stroke: '#ff0000' /* red */}, new go.Binding('opacity', 'opacity'))
       ));
@@ -1186,6 +1184,58 @@ class GenogramTree extends React.Component {
       };
     }
 
+    findCircle(x1,  y1,  x2,  y2, x3, y3)
+    {
+        var x12 = (x1 - x2);
+        var x13 = (x1 - x3);
+    
+        var y12 =( y1 - y2);
+        var y13 = (y1 - y3);
+    
+        var y31 = (y3 - y1);
+        var y21 = (y2 - y1);
+    
+        var x31 = (x3 - x1);
+        var x21 = (x2 - x1);
+    
+        //x1^2 - x3^2
+        var sx13 = Math.pow(x1, 2) - Math.pow(x3, 2);
+    
+        // y1^2 - y3^2
+        var sy13 = Math.pow(y1, 2) - Math.pow(y3, 2);
+    
+        var sx21 = Math.pow(x2, 2) - Math.pow(x1, 2);
+        var sy21 = Math.pow(y2, 2) - Math.pow(y1, 2);
+    
+        var f = ((sx13) * (x12)
+                + (sy13) * (x12)
+                + (sx21) * (x13)
+                + (sy21) * (x13))
+                / (2 * ((y31) * (x12) - (y21) * (x13)));
+        var g = ((sx13) * (y12)
+                + (sy13) * (y12)
+                + (sx21) * (y13)
+                + (sy21) * (y13))
+                / (2 * ((x31) * (y12) - (x21) * (y13)));
+    
+        var c = -(Math.pow(x1, 2)) -
+        Math.pow(y1, 2) - 2 * g * x1 - 2 * f * y1;
+    
+        // eqn of circle be
+        // x^2 + y^2 + 2*g*x + 2*f*y + c = 0
+        // where centre is (h = -g, k = -f) and radius r
+        // as r^2 = h^2 + k^2 - c
+        var h = -g;
+        var k = -f;
+        var sqr_of_r = h * h + k * k - c;
+    
+        // r is the radius
+        var r = Math.sqrt(sqr_of_r);
+        return {
+          cX: h, cY: k, r: r
+        };
+    }
+
     computePoints() {
       var fromnode = this.fromNode;
       if (!fromnode) return false;
@@ -1215,18 +1265,27 @@ class GenogramTree extends React.Component {
       var dia = Math.sqrt((fx - tx) * (fx - tx) + (fy - ty) * (fy - ty));
       let height = Math.min(30, dia / 5);
 
-      let X = (rotatedFromPoint.x + rotatedToPoint.x) / 2;
-      let Y = rotatedFromPoint.y + height * ((fx > tx) != (this.fromSpot === go.Spot.Bottom) ? 1 : -1);
-      let secondPoint = this.rotate(
-        X, 
-        Y,
-        slopeAngle
-        );
-      this.addPointAt(secondPoint.x, secondPoint.y);
+      let midX = (rotatedFromPoint.x + rotatedToPoint.x) / 2;
+      let midY = rotatedFromPoint.y + height * ((fx > tx) != (this.fromSpot === go.Spot.Bottom) ? 1 : -1);
+
+      let circle = this.findCircle(rotatedFromPoint.x, rotatedFromPoint.y, rotatedToPoint.x, rotatedToPoint.y, midX, midY);
+      let parametric = (t) => {
+        return {
+          x: circle.r * Math.cos(t) + circle.cX,
+          y: circle.r * Math.sin(t) + circle.cY
+        };
+      };
+      let startAngle = Math.atan2(rotatedFromPoint.y - circle.cY, rotatedFromPoint.x - circle.cX);
+      let endAngle = Math.atan2(rotatedToPoint.y - circle.cY, rotatedToPoint.x - circle.cX);
+      let increment = (endAngle - startAngle) / 10;
+      for (var currentAngle = startAngle ; 
+          startAngle < endAngle ? currentAngle <= endAngle : currentAngle >= endAngle ; 
+          currentAngle += increment) {
+            let parametricResult = parametric(currentAngle);
+            let point = this.rotate(parametricResult.x, parametricResult.y, slopeAngle);
+            this.addPointAt(point.x, point.y);
+          }
       this.addPoint(topoint);
-
-
-      let radius = (height * height + dia * dia / 4) / (2 * height);
       this.updateTargetBindings();
       return true;
     }
