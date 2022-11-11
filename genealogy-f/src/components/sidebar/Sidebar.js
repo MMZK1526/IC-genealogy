1ec38bbd003b6f5edb62b49074b44b167c0d6107
mm2320 <mm2320@ic.ac.uk>
2022-11-11 14:26:17 +0000

    [mm2320] (UI) rearrange sidebar UI

--- genealogy-f/src/components/sidebar/Sidebar.js
+++ genealogy-f/src/components/sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 
 import { Form } from 'react-bootstrap';
-import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import './Sidebar.css';
 import Multiselect from 'multiselect-react-dropdown';
@@ -12,37 +11,47 @@ const names = [
 ];
 
 export function Sidebar(props) {
-  return (
-    <div className='sidebar'>
-      <Form>
-        <Multiselect
-            options={Array.from(props.filters.allFamilies).map((v) => ({name: v, id: v}))} // Options to display in the dropdown
-            selectedValues={Array.from(props.filters.families).map((v) => ({name: v, id: v}))} // Preselected value to persist in dropdown
-            onSelect={(_, i) => props.filters.families.add(i.name)} // Function will trigger on select event
-            onRemove={(_, i) => props.filters.families.delete(i.name)} // Function will trigger on remove event
-            displayValue='name' // Property name to display in the dropdown options
-        />
+    return (
+        <div className='sidebar'>
+            <Form>
+                <Form.Label className="label">Family: </Form.Label>
+                <Multiselect
+                    options={Array.from(props.filters.allFamilies).map((v) => ({name: v, id: v}))} // Options to display in the dropdown
+                    selectedValues={Array.from(props.filters.families).map((v) => ({name: v, id: v}))} // Preselected value to persist in dropdown
+                    onSelect={(_, i) => props.filters.families.add(i.name)} // Function will trigger on select event
+                    onRemove={(_, i) => props.filters.families.delete(i.name)} // Function will trigger on remove event
+                    displayValue='name' // Property name to display in the dropdown options
+                />
 
-        <Form.Group className='form-group' controlId='bloodline-checkbox'>
-          <Form.Check title='Bloodline' label='Bloodline Only' type='checkbox' defaultChecked 
-          onChange={(e) => {
-            props.filters.bloodline = e.target.checked;
-          }}
-          />
-        </Form.Group>
+                <Form.Group className='form-group' controlId='bloodline-checkbox'>
+                    <Form.Check title='Bloodline' label='Bloodline only' type='checkbox' defaultChecked 
+                    onChange={(e) => {
+                        props.filters.bloodline = e.target.checked;
+                    }}
+                />
+            </Form.Group>
 
-        <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={names}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Search for name" />}
-        />
+                <Button onClick={() => props.onChange()}>
+                    Apply
+                </Button>
+            </Form>
 
-        <Button onClick={() => props.onChange()}>
-          Apply
-        </Button>
-      </Form>
-    </div>
-  );
+            <hr style={{
+                background: 'darkgrey',
+                height: '2px',
+                width: '100%',
+                border: 'none',
+                margin: '20px 0px'
+            }} />
+
+            <label className="label">Search for name in tree: </label>
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={names}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} />}
+            />
+        </div>
+    );
 }
