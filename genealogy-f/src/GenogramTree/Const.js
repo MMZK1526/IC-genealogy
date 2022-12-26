b2c10bd6fd90b0045d92660020a5b1f79daf5f6a
mmzk1526 <yc4120@ic.ac.uk>
2022-12-26 18:12:58 +0000

    [mmzk] (refactor) Delete utilFunctions.js

--- genealogy-f/src/GenogramTree/Const.js
+++ genealogy-f/src/GenogramTree/Const.js
@@ -4,3 +4,9 @@ export class Opacity {
     static outlier = '0.5';
     static hidden = '0.2';
 }
+
+// global map from id of person to their attributes, used to change opacity for filtering in the goJs diagram.
+export let relMap = new Map();
+export const setRelMap = (newMap) => {
+    relMap = newMap;
+}
