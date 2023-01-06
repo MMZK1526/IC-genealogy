edd0b25c12162b5284fd81485846f7d72676dd2e
mmzk1526 <yc4120@ic.ac.uk>
2023-01-06 13:16:02 +0000

    [mmzk] (test) Unit tests for relations_db

--- genealogy-b/src/test/kotlin/mmzk/genealogy/ApplicationTest.kt
+++ genealogy-b/src/test/kotlin/mmzk/genealogy/ApplicationTest.kt
@@ -5,7 +5,7 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import mmzk.genealogy.plugins.configureRouting
+import kotlinx.serialization.json.*
 
 class ApplicationTest {
     @Test
@@ -27,5 +27,34 @@ class ApplicationTest {
         val wdID = "_Q12"
         val parsed = Fields.parseID(wdID)
         assertEquals(null, parsed)
+    }
+
+    @Test
+    fun `depth 0 only has self and spouse as result`() = testApplication {
+        val client = createClient {}
+        client.post("/relations_db?depth=0&id=WD-Q9682") {
+            contentType(ContentType.Application.Json)
+            setBody("[]")
+        }.apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals(2, Json.parseToJsonElement(bodyAsText()).jsonObject["items"]?.jsonObject?.keys?.size)
+        }
+    }
+
+    @Test
+    fun `depth 2 for QEII contains Mary of Teck but not Edward VIII`() = testApplication {
+        val client = createClient {}
+        client.post("/relations_db?depth=2&id=WD-Q9682") {
+            contentType(ContentType.Application.Json)
+            setBody("[]")
+        }.apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertTrue(
+                Json.parseToJsonElement(bodyAsText()).jsonObject["items"]?.jsonObject?.containsKey("WD-Q76927") ?: false
+            )
+            assertFalse(
+                Json.parseToJsonElement(bodyAsText()).jsonObject["items"]?.jsonObject?.containsKey("WD-Q20875") ?: true
+            )
+        }
     }
 }
